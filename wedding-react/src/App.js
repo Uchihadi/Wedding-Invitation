42f9cde4477333ff1ddbfee8185af258a279903e
Nurhadi Abdul Rahman <nurhadi.rahman@icompaz.com>
2023-09-06 12:40:07 +0800

    Tailwind CSS Set up

--- wedding-react/src/App.js
+++ wedding-react/src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="App bg-red-500 min-h-screen">
+        <p className='ml-96'>
+          Wedding Invitation Hello
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
