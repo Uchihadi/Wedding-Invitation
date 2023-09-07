1219cb650e0eeedb64537700e0a02df638da96ca
Nurhadi Abdul Rahman <nurhadi.rahman@icompaz.com>
2023-09-07 12:19:27 +0800

    Porting over to individual Component

--- wedding-react/src/App.js
+++ wedding-react/src/App.js
@@ -2,69 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import './index.css';
 import Navbar from './Navbar';
+import Title from './Title';
 import { FaLocationDot } from "react-icons/fa6";
 import { FaRegCalendarCheck } from "react-icons/fa6";
 import { FaShareNodes } from "react-icons/fa6";
+import Solemnisation from './Solemnisation';
+import Reception from './Reception';
+import Guestbook from './Guestbook';
 
 function App() {
   return (
     <>
     <body>
       <Navbar/>
-      {/* TITLE */}
-      <div className="hero">
-        <h2>The Wedding Of</h2>
-        <h1>Nurhadi & Nurdiyahnah</h1>
-        <h3>Saturday, 23 December 2023</h3>
-      </div>
-
-      {/* SOLEMNISATION */}
-      <section id="akad">
-      <div className="bingkai">
-        <div className="line"></div>
-        <h1>Solemnisation</h1>
-        <div className="line"></div>
-      </div>
-      <div className="section_wrap wrap">
-        <div className="section_container_akad container">
-          <h3>Saturday, 23 December 2023</h3>
-          <h3>Time: 1100</h3>
-          <h4>Bijan B'Majestic, Grandlink Square, 511 Guillemard Road</h4>
-          <h4>Singapore</h4>
-        </div>
-        <section className="akad_img"></section>
-      </div>
-      </section>
-      <div className="bouquet_container">
-        <img
-          className="bouquet"
-          src="https://cdn.pixabay.com/photo/2020/09/01/18/13/background-5535928_960_720.png"
-          alt=""
-        />
-      </div>
-
-      {/* RECEPTION */}
-      <section id="resepsi">
-      <div className="bingkai">
-        <div className="line"></div>
-        <h1>Reception</h1>
-        <div className="line"></div>
-      </div>
-      <div className="section_wrap wrap">
-        <section className="resepsi_img"></section>
-        <div className="section_container_resepsi">
-          <h3>Saturday, 23 December 2023</h3>
-          <h3>Time: 1200</h3>
-          <h4>Bijan B'Majestic, Grandlink Square, 511 Guillemard Road</h4>
-          <h4>Singapore</h4>
-        </div>
-      </div>
-    </section>
-    <img 
-      className="bouquet2"
-      src="https://cdn.pixabay.com/photo/2016/06/27/14/42/flowers-1482644_960_720.png"
-      alt=""
-    />
+      <Title/>
+      <Solemnisation/>
+      <Reception/>
+      <Guestbook/>
 
     {/* GUEST BOOK */}
     <section id="buku_tamu">
---
+++ wedding-react/src/Guestbook.js
@@ -0,0 +1,34 @@
+import "./App.css";
+
+function Guestbook () {
+    return (
+    <section id="buku_tamu">
+      <div className="bingkai">
+        <div className="line"></div>
+        <h1>Guestbook</h1>
+        <div className="line"></div>
+      </div>
+      <div className="buku_tamu_container">
+        <form action="">
+          <input required type="text" placeholder="Name" />
+          <textarea
+            name="Well Wishes"
+            placeholder="Please write your well wishes here!"
+            cols="30"
+            rows="8"
+          ></textarea>
+          <select name="kehadiran">
+            <option value="hadir">Attending</option>
+            <option value="tidak_hadir">Won't be Attending</option>
+          </select>
+
+          <div className="btn">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    </section>
+    )
+}
+
+export default Guestbook;
