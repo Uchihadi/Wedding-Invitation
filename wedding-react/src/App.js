d11cc9004471a5a8b68e93bd8e36949f7c898cae
Nurhadi Abdul Rahman <nurhadi.rahman@icompaz.com>
2023-09-08 12:12:16 +0800

    Footer & GMap

--- wedding-react/src/App.js
+++ wedding-react/src/App.js
@@ -3,12 +3,11 @@ import './App.css';
 import './index.css';
 import Navbar from './Navbar';
 import Title from './Title';
-import { FaLocationDot } from "react-icons/fa6";
-import { FaRegCalendarCheck } from "react-icons/fa6";
-import { FaShareNodes } from "react-icons/fa6";
 import Solemnisation from './Solemnisation';
 import Reception from './Reception';
 import Guestbook from './Guestbook';
+import GMap from './GMap';
+import Footer from './Footer';
 
 function App() {
   return (
@@ -19,48 +18,8 @@ function App() {
       <Solemnisation/>
       <Reception/>
       <Guestbook/>
-
-    {/* GOOGLE MAP */}
-    <section id="map">
-      <div className="bingkai">
-        <div className="line"></div>
-        <h1>Map</h1>
-        <div className="line"></div>
-      </div>
-      <div className="section_wrap wrap">
-        <div className="section_container_map">
-          <button className="map_list">
-            <span className="material-symbols-outlined"> <FaLocationDot/> </span>
-            Wedding Location
-          </button>
-          <button className="map_list">
-            <span className="material-symbols-outlined"> <FaRegCalendarCheck/> </span>
-            Save The Calendar
-          </button>
-          <button className="map_list">
-            <span className="material-symbols-outlined"> <FaShareNodes/> </span>
-            Share On Instagram
-          </button>
-        </div>
-        <section className="map_img">
-          <iframe
-          src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJBe1nR0QEBBARewvYudV7Yww&key=AIzaSyBExIl5Az1PFZWB47BdeG2qQh_uDymbDYU"
-            width="600"
-            height="350"
-            // style="border: 0"
-            allowfullscreen=""
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </section>
-      </div>
-    </section>
-
-    {/* FOOTER */}
-    <footer>
-      <p>Design By</p>
-      <h3>Nurhadi Bin Abdul Rahman</h3>
-    </footer>
+      <GMap/>
+      <Footer/>
     </body>
     </>
    
